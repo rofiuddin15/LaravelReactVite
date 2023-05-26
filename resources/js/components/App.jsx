da3dcf09d0d977d2ffd6fb7a05886667e0fa6eb9
rofiareiv <kambing.rofi@gmail.com>
2023-05-26 09:01:06 +0700

    setup awal laravel reactjs.

---
+++ resources/js/components/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div>
+      <h1>Halao React Laravel</h1>
+      <h2>ini adalah halaman utama react</h2>
+    </div>
+  )
+}
+
+export default App;
